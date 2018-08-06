cd365c6665b61a5ed31d9382837f4eb371455ec7
vinu <danbei@gmail.com>
2018-08-06 14:57:04 +0900

    초기 커밋.

---
+++ ExpressApp1/routes/index.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var getData = function () {
+    var data = {
+        'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
+        'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
+        'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
+    };
+    return data;
+};
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' , "data": getData() });
+});
+
+module.exports = router;
